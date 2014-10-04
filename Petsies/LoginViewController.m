68de0c9a583b992c0efb668bff15dcc7876fbaf9
yoonsw15 <yoonsw15@ucla.edu>
2014-10-04 13:37:05 -0400

    first commit

---
+++ Petsies/LoginViewController.m
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.m
+//  Petsies
+//
+//  Created by SeungWon on 2014. 10. 3..
+//  Copyright (c) 2014년 BruinCS. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    NSLog(@"I'm loaded");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Petsies/RegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.h
+//  Petsies
+//
+//  Created by SeungWon on 2014. 10. 3..
+//  Copyright (c) 2014년 BruinCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@end
