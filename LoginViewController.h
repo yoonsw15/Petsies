1998aa520786057857b145c6df192430ad34970f
Jennifer Jo <jenniferjo@Jennifers-MacBook-Pro-2.local>
2014-10-04 16:42:16 -0400

    loginview added

--- LoginViewController.h
+++ LoginViewController.h
@@ -2,8 +2,8 @@
 //  LoginViewController.h
 //  Petsies
 //
-//  Created by SeungWon on 2014. 10. 3..
-//  Copyright (c) 2014년 BruinCS. All rights reserved.
+//  Created by Jennifer Jo on 10/4/14.
+//  Copyright (c) 2014 BruinCS. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
---
+++ LoginViewController.m
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.m
+//  Petsies
+//
+//  Created by Jennifer Jo on 10/4/14.
+//  Copyright (c) 2014 BruinCS. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@implementation LoginViewController
+
+@end
